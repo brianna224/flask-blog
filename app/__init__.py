963696eb2e0dd727cbfa8e0676370de840b46aac
Brianna Patrick <briannapatrick2020@gmail.com>
2021-07-08 11:02:42 -0400

    login/register changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,32 @@
 import os
 import json
-from flask import Flask, request, render_template
-#from dotenv import load_dotenv
+from flask import Flask, request, render_template, url_for
+
+# from dotenv import load_dotenv
 
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -34,71 +38,84 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-#load_dotenv()
+
+# load_dotenv()
 
 f = open("./lavina.json")
 data = json.load(f)
 f.close()
 
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv('URL'))
 
-@app.route('/health')
+@app.route("/")
+def index():
+    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route("/health")
 def health():
     return render_template(
-        'data.html',
+        "data.html",
         data=data,
         main="Accomplishments",
         side1="Experience",
         side2="Projects",
-        url=os.getenv("URL"))
+        url=os.getenv("URL"),
+    )
 
 
-#@app.route('/')
-@app.route('/Experience/')
+# @app.route("/")
+@app.route("/Experience/")
 def experience():
-    return render_template('data.html',
-                           data=data,
-                           main="Experience",
-                           side1="Projects",
-                           side2="Accomplishments",
-                           url=os.getenv("URL"))
+    return render_template(
+        "data.html",
+        data=data,
+        main="Experience",
+        side1="Projects",
+        side2="Accomplishments",
+        url=os.getenv("URL"),
+    )
 
-@app.route('/Projects/')
+
+@app.route("/Projects/")
 def projects():
-    return render_template('data.html',
-                           data=data,
-                           main="Projects",
-                           side1="Experience",
-                           side2="Accomplishments",
-                           url=os.getenv("URL"))
+    return render_template(
+        "data.html",
+        data=data,
+        main="Projects",
+        side1="Experience",
+        side2="Accomplishments",
+        url=os.getenv("URL"),
+    )
 
-@app.route('/Accomplishments/')
+
+@app.route("/Accomplishments/")
 def accomplishments():
-    return render_template('data.html',
-                           data=data,
-                           main="Accomplishments",
-                           side1="Experience",
-                           side2="Projects",
-                           url=os.getenv("URL"))
+    return render_template(
+        "data.html",
+        data=data,
+        main="Accomplishments",
+        side1="Experience",
+        side2="Projects",
+        url=os.getenv("URL"),
+    )
+
 
 # for database
-@app.route('/register', methods=('GET', 'POST'))
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        #db = get_db()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # db = get_db()
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
-         
+
         if error is None:
             new_user = UserModel(username, generate_password_hash(password))
             db.session.add(new_user)
@@ -109,26 +126,26 @@ def register():
             return error, 418
 
     ## TODO: Return a restister page
-    return render_template("login.html", data=data, mode="register")
+    return render_template("register.html", title="register")
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         error = None
-        
+
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
 
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
         if error is None:
-            return "Login Successful", 200 
+            return "Login Successful", 200
         else:
             return error, 418
-    
+
     ## TODO: Return a login page
     return render_template("login.html", data=data, mode="login")
